c0ec9917fab1aa98c4b8bfd9006e144ace5817e3
Aparnahegde <aparnaghegde10@gmail.com>
2025-02-16 20:45:11 +0530

    SynatxError and ReferenceError using let

---
+++ let&const/script.js
@@ -0,0 +1,23 @@
+// console.log(a); 
+// let a=10;// ReferenceError: Cannot access 'a' before initialization
+
+// console.log(this.a);                                     //undefined
+// var a=10;
+
+
+// console.log(this.a);                                     //undefined
+// let a=10;
+
+
+// let a=10;
+// let a=38; //SyntaxError: Identifier 'a' has already been declared
+
+
+// console.log("abx");
+// let a=10;
+// let a=100; //SyntaxError: Identifier 'a' has already been declared
+
+
+// console.log("abx");
+// let a=10;
+// var a=100;//SyntaxError: Identifier 'a' has already been declared
