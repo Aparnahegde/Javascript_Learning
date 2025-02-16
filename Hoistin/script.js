7e40fdceb74fba268a3f05ec2b4bf8f4911db08f
Aparnahegde <aparnaghegde10@gmail.com>
2025-02-16 11:57:25 +0530

    some more example of hoisting

---
+++ Hoistin/script.js
@@ -0,0 +1,41 @@
+// getName();
+// console.log(x);
+// console.log(getName);
+
+// var x=10;   
+// function getName(){
+//     console.log("namaste javascript"); 
+// }
+
+
+
+
+// getName();
+// console.log(x);
+// console.log(getName);
+
+// var x=10;   
+// var getName=function (){                                              //getName also become variable so we can't call it before declaration  
+//     console.log("namaste javascript"); 
+// }
+
+
+
+// console.log(x);
+// console.log(getName);
+
+// var x=10;   
+// var getName=function (){                                               
+//     console.log("namaste javascript"); 
+// }
+// getName();
+
+getName();
+console.log(x);
+console.log(getName);
+
+var x=10;   
+var getName=() =>{                                               
+    console.log("namaste javascript"); 
+}
+
