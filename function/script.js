7760d9d25173a81b2094cc3f1ef304ce382ded95
Aparnahegde <aparnaghegde10@gmail.com>
2025-02-16 11:48:33 +0530

    function and hoisting

---
+++ function/script.js
@@ -0,0 +1,14 @@
+    
+        getName();
+        // getName1();
+        console.log(x);
+        console.log(getName);
+
+        var x=10;
+        function getName(){
+    console.log("namaste javascript"); 
+    }
+
+    var getName1=function (){
+        console.log("namaste1 javascript");
+    }
