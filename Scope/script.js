2cb2c2248a91544b60f013cf805f2ed2fc3e9106
Aparnahegde <aparnaghegde10@gmail.com>
2025-02-16 18:00:00 +0530

    lexographycal nested function hierarchy

--- Scope/script.js
+++ Scope/script.js
@@ -6,31 +6,31 @@
 // a();
 
 
-// function a() {
-//   c();
-//    function c() {
-//      console.log(b);
-//    }  
-//   }   
-  
-//   var b=10;
-//   a();
-  
-
-
-
-
-
 function a() {
-  var b=10;
   c();
    function c() {
-    
+     console.log(b);
    }  
   }   
-  console.log(b);               // ReferenceError: b is not defined
   
+  var b=10;
   a();
+  
+
+
+
+
+
+// function a() {
+//   var b=10;
+//   c();
+//    function c() {
+    
+//    }  
+//   }   
+//   console.log(b);               // ReferenceError: b is not defined
+  
+//   a();
   
   
 
