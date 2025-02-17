305426d9cbf6ee7d0db8da59f0d65217c33354bf
Aparnahegde <aparnaghegde10@gmail.com>
2025-02-17 15:11:02 +0530

    lexical scope const

--- Block/script.js
+++ Block/script.js
@@ -2,10 +2,37 @@
 //     console.log('Hello World'); //Hello World
 // }
 
+// var a=100;
+// {
+//     var a=10;
+//     let b=20;
+//     const c=30;
+// }
+// console.log(a); 
 
+
+// var a=100;
+// {
+//     var a=10;
+//     console.log("a inside block "+a);
+// }
+// console.log("a outside block "+a); 
+
+
+// let a=100;
+// {
+//     let a=10;
+//     console.log("a inside block "+a);
+// }
+// console.log("a outside block "+a);
+
+
+
+const a=20;
 {
-    var a=10;
-    let b=20;
-    const c=30;
+   // const a=10;
+    {
+        // const a=5;
+        console.log("a inside block "+a);  
+    }
 }
-var x=20;
