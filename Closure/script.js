f30f01f54b7165bf8d9d2e51f128ac1e4c17492a
Aparnahegde <aparnaghegde10@gmail.com>
2025-02-17 18:26:58 +0530

    nested function closure

--- Closure/script.js
+++ Closure/script.js
@@ -1,7 +1,46 @@
-function x(){
-    var a = 10;
-    function y(){
-        console.log(a);
-    }
-    y();    // 10
-}
+// function x(){
+//     var a = 10;
+//     function y(){
+//         console.log(a);
+//     }
+//     y();    // 10
+// }
+
+
+// function x(){
+//     var a = 10;
+//     function y(){
+//         console.log(a);
+//     }
+//    return y; 
+// }
+// var z= x();
+// console.log(z);  // ƒ y(){  console.log(a);  }  
+// z();  // 10
+
+
+// function x(){
+//     var a = 10;
+//     function y(){
+//         console.log(a);
+//     }
+//     a=100;
+//    return y; 
+// }
+// var z= x();
+// console.log(z);  // ƒ y(){  console.log(a);  }  
+// z(); // 100
+
+// function z(){
+//     var b=900;
+//     function x(){
+//             var a = 10;
+//             function y(){
+//                 console.log(a,b);
+//             }
+//             y();   // 10 900
+//         }
+//         x();// 10 900
+// }
+// z();//
+
