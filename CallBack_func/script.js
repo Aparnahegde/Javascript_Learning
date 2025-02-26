849cbbd2bc5e15eeb05305bc758fe3c6f6583e26
Aparnahegde <aparnaghegde10@gmail.com>
2025-02-26 10:48:32 +0530

    listener example

--- CallBack_func/script.js
+++ CallBack_func/script.js
@@ -1,13 +1,19 @@
-setTimeout(function(){
-    console.log('timer');
-}, 3000);
+// setTimeout(function(){
+//     console.log('timer');
+// }, 3000);
 
-function x(y){
-    console.log('x');
-    y();    
+// function x(y){
+//     console.log('x');
+//     y();    
 
-}
-x(function y(){
-    console.log('Hello Y');
-})
+// }
+// x(function y(){
+//     console.log('Hello Y');
+// })
+
+
+//eventlistener
+document.getElementById("clickme").addEventListener('click',function xyz(){
+   alert('Button Clicked');
+}); 
 
