4d7fd4f6457876d682d1332ae5fbc42b40716083
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-02 17:21:55 +0530

    reduce function

--- MAP_FILTER_reduce/script.js
+++ MAP_FILTER_reduce/script.js
@@ -25,22 +25,39 @@
 // console.log(outputsq);
 
 
+// const arr = [1,2,3,4,5];
+
+// function isOdd(x){
+//     return x%2;
+// }
+// function isEven(x){ 
+//     return x%2==0;
+// }
+
+// const outputOdd=arr.filter(isOdd)
+// console.log(outputOdd); // [1,3,5]
+
+// const outputEven=arr.filter(isEven)
+// console.log(outputEven); // [2,4]
+
+// const outputgreaterthan2=arr.filter((x)=>x>2)
+// console.log(outputgreaterthan2); // [3,4,5]
+
 const arr = [1,2,3,4,5];
+const sum=arr.reduce((acc,curr)=>acc+curr,0)
+console.log(sum); // 15     
 
-function isOdd(x){
-    return x%2;
-}
-function isEven(x){ 
-    return x%2==0;
-}
+// const max=arr.reduce((acc,curr)=>Math.max(acc,curr),0)
+// console.log(max); // 5
 
-const outputOdd=arr.filter(isOdd)
-console.log(outputOdd); // [1,3,5]
+const output=arr.reduce(function(acc,curr){
+    if(acc<curr){
+        acc=curr;
+    }
+return acc;
+},0)
+console.log(output); // 5   
 
-const outputEven=arr.filter(isEven)
-console.log(outputEven); // [2,4]
 
-const outputgreaterthan2=arr.filter((x)=>x>2)
-console.log(outputgreaterthan2); // [3,4,5]
 
 
