12894ad179b2a73eca6b3ed6c2f5e9b42895153d
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-02 11:22:16 +0530

    modular coding

--- higher_order_func/script.js
+++ higher_order_func/script.js
@@ -1,8 +1,49 @@
-function x(){
-    console.log("I am x");
+// function x(){
+//     console.log("I am x");
+// }
+
+// function y(x){
+//     console.log("I am y");
+//     x();
+// } 
+
+// y(x);
+
+
+// const r=[1,2,3,4];
+
+// const calculateArea=function(r){
+//     const output=[];
+//     for(let i=0;i<r.length;i++){
+//         output.push(3.14*r[i]*r[i]);
+//     }   
+//     return output;
+// }
+// console.log(calculateArea(r));
+
+
+const r=[1,2,3,4];
+
+const area=function(r){
+    return 3.14*r*r;
+}   
+
+const circumference=function(r){
+    return 2*3.14*r;
 }
 
-function y(x){
-    console.log("I am y");
-    x();
-} 
+const diameter=function(r){
+    return 2*r;
+}
+
+
+const calculate=function(r,logic){
+    const output=[];
+    for(let i=0;i<r.length;i++){
+        output.push(logic(r[i]));
+    }   
+    return output;
+}
+console.log("AREA: "+calculate(r,area));
+console.log("CIRCUMFERENCE: "+calculate(r,circumference));
+console.log("DIAMETER: "+calculate(r,diameter));
