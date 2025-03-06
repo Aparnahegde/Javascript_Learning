ba77ddc73b38eb377fcba7065fd6446dc5dfe30a
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-07 00:00:16 +0530

    how promise chain works,and solves call back hell

--- Promises/script.js
+++ Promises/script.js
@@ -1,9 +1,35 @@
-const cart=["shoes","shirts","pants"];  
+// const cart=["shoes","shirts","pants"];  
+// createOrder(cart,function(orderID){
+//     proceedToPayment(paymentID)
+// });
+
+// const promise=createOrder(cart);
+// promise.then(function(orderID){
+//     proceedToPayment(paymentID);
+// });
+
+
+// const GITHUB_API="https://api.github.com/users/Aparnahegde";
+// const user=fetch(GITHUB_API);
+// console.log(user);
+
+// user.then(function(data){
+//     console.log(data);
+// });
+
 createOrder(cart,function(orderID){
-    proceedToPayment(paymentID)
+    proceedToPayment(orderID,function(paymentID){
+        showSummary(paymentID,function(){
+            updateWallet();
+        });
+    });
 });
 
-const promise=createOrder(cart);
-promise.then(function(orderID){
-    proceedToPayment(paymentID);
+createOrder(cart).then(function(orderID){
+    return proceedToPayment(orderID);
+}).then(function(paymentID){
+    return showSummary(paymentID);
+}).then(function(){
+    return updateWallet();
 });
+
