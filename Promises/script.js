2122a33166d83aa78b15e93b4f881e4e44d0667a
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-07 23:14:45 +0530

    with failed exmple for all()

--- Promises/script.js
+++ Promises/script.js
@@ -37,6 +37,25 @@
 // .then(paymentID=>showSummary(paymentID))    
 // .then(()=>updateWallet());
 
+// const p1=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve("Promise 1 resolved");
+//     },3000);
+// })
+// const p2=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         resolve("Promise 2 resolved");
+//     },1000);
+// })
+// const p3=new Promise(function(resolve,reject){  
+//     setTimeout(function(){
+//         resolve("Promise 3 resolved");
+//     },2000);
+// }   )
+
+// Promise.all([p1,p2,p3]).then(res => console.log(res));
+
+
 const p1=new Promise(function(resolve,reject){
     setTimeout(function(){
         resolve("Promise 1 resolved");
@@ -44,15 +63,15 @@ const p1=new Promise(function(resolve,reject){
 })
 const p2=new Promise(function(resolve,reject){
     setTimeout(function(){
-        resolve("Promise 2 resolved");
+        reject("Promise 2 rejected");
     },1000);
 })
 const p3=new Promise(function(resolve,reject){  
     setTimeout(function(){
-        resolve("Promise 3 resolved");
+        reject("Promise 3 rejected");
     },2000);
 }   )
 
-Promise.all([p1,p2,p3]).then(res => console.log(res));
+Promise.all([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
 
 
