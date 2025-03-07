a461ae0533db4f27a0fb90e0bc743ea33ca9161d
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-07 23:45:34 +0530

    aggregate error

--- Promises/script.js
+++ Promises/script.js
@@ -152,3 +152,20 @@
 //         Promise.any([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
 
 
+const p1=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        reject("Promise 1 rejected");
+    },3000);
+})
+const p2=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        reject("Promise 2 rejected");
+    },5000);
+})
+const p3=new Promise(function(resolve,reject){  
+    setTimeout(function(){
+        reject("Promise 3 rejected");
+    },2000);
+}   )
+
+Promise.any([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
