2e3fecd76e684e6fee1253d3298c355d6662eda4
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-07 23:44:56 +0530

    any func

--- Promises/script.js
+++ Promises/script.js
@@ -113,20 +113,42 @@
 
 // Promise.allSettled([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
 
-const p1=new Promise(function(resolve,reject){
-        setTimeout(function(){
-            resolve("Promise 1 resolved");
-        },3000);
-    })
-    const p2=new Promise(function(resolve,reject){
-        setTimeout(function(){
-            reject("Promise 2 rejected");
-        },5000);
-    })
-    const p3=new Promise(function(resolve,reject){  
-        setTimeout(function(){
-            reject("Promise 3 rejected");
-        },2000);
-    }   )
+// const p1=new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             resolve("Promise 1 resolved");
+//         },3000);
+//     })
+//     const p2=new Promise(function(resolve,reject){
+//         setTimeout(function(){
+//             reject("Promise 2 rejected");
+//         },5000);
+//     })
+//     const p3=new Promise(function(resolve,reject){  
+//         setTimeout(function(){
+//             reject("Promise 3 rejected");
+//         },2000);
+//     }   )
     
-    Promise.race([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
+//     Promise.race([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
+
+
+
+// const p1=new Promise(function(resolve,reject){
+//             setTimeout(function(){
+//                 resolve("Promise 1 resolved");
+//             },3000);
+//         })
+//         const p2=new Promise(function(resolve,reject){
+//             setTimeout(function(){
+//                 reject("Promise 2 rejected");
+//             },5000);
+//         })
+//         const p3=new Promise(function(resolve,reject){  
+//             setTimeout(function(){
+//                 reject("Promise 3 rejected");
+//             },2000);
+//         }   )
+        
+//         Promise.any([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
+
+
