34caf99da2e534cb777a35d99f56f780cf9a0375
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-09 10:32:26 +0530

    last commit

--- Promises/script.js
+++ Promises/script.js
@@ -113,6 +113,54 @@
 
 // Promise.allSettled([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
 
+
+
+
+
+
+
+
+const p1=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        reject("Promise 1 rejected");
+    },3000);
+})
+const p2=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        reject("Promise 2 rejected");
+    },5000);
+})
+const p3=new Promise(function(resolve,reject){  
+    setTimeout(function(){
+        reject("Promise 3 rejected");
+    },2000);
+}   )
+
+Promise.allSettled([p1,p2,p3]).then(res => console.log(res))
+.catch(err=>{
+    console.error(err)
+    console.log(err.errors);
+}
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const p1=new Promise(function(resolve,reject){
 //         setTimeout(function(){
 //             resolve("Promise 1 resolved");
@@ -152,25 +200,25 @@
 //         Promise.any([p1,p2,p3]).then(res => console.log(res)).catch(err=>console.error(err));
 
 
-const p1=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        reject("Promise 1 rejected");
-    },3000);
-})
-const p2=new Promise(function(resolve,reject){
-    setTimeout(function(){
-        reject("Promise 2 rejected");
-    },5000);
-})
-const p3=new Promise(function(resolve,reject){  
-    setTimeout(function(){
-        reject("Promise 3 rejected");
-    },2000);
-}   )
+// const p1=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         reject("Promise 1 rejected");
+//     },3000);
+// })
+// const p2=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         reject("Promise 2 rejected");
+//     },5000);
+// })
+// const p3=new Promise(function(resolve,reject){  
+//     setTimeout(function(){
+//         reject("Promise 3 rejected");
+//     },2000);
+// }   )
 
-Promise.any([p1,p2,p3]).then(res => console.log(res))
-.catch(err=>{
-    console.error(err)
-    console.log(err.errors);
-}
-);
+// Promise.any([p1,p2,p3]).then(res => console.log(res))
+// .catch(err=>{
+//     console.error(err)
+//     console.log(err.errors);
+// }
+// );
