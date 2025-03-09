f946cc0d12c352e622957d7214c806ac5542bacf
Aparnahegde <aparnaghegde10@gmail.com>
2025-03-09 23:33:27 +0530

    promise async await

--- async_await/script.js
+++ async_await/script.js
@@ -11,17 +11,46 @@
 
 
 
+// const p=new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//         resolve("Hello World")
+//     },2000)
+// })
+// async function getdata(){
+//     return p;
+//     }
+    
+//     const data = getdata();
+//     console.log(data); // Promise {<pending>}
+    
+//     const dataPromise = getdata();  
+//     dataPromise.then(data => console.log(data)); // Hello World
+
+
+
+
+
+
+
+
+
 const p=new Promise((resolve,reject)=>{
     setTimeout(()=>{
-        resolve("Hello World")
-    },2000)
-})
-async function getdata(){
-    return p;
-    }
+        resolve("Promise Resolved");
+    },10000);
+}
+);
+// async function handlePromise(){
+//     const result=await p; //await only be used inside async function
+//     console.log(result);
+// }
+// handlePromise(); //Promise Resolved
+
+function getData(){
+    p.then((res)=> console.log(res))
+        console.log("Namaste JavaScript");
     
-    const data = getdata();
-    console.log(data); // Promise {<pending>}
-    
-    const dataPromise = getdata();  
-    dataPromise.then(data => console.log(data)); // Hello World
+}
+getData();
+
+
